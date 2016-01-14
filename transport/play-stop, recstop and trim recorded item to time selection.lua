90b1055e1d9975640f6bf50244567b3104d9823b
me2beats <prod.me2beats@gmail.com>
2016-01-14 21:34:41 +0500

    +

---
+++ transport/play-stop, recstop and trim recorded item to time selection.lua
@@ -0,0 +1,34 @@
+script_title = "play-stop, recstop and trim recorded item to time selection"
+reaper.Undo_BeginBlock()
+
+function SaveCursorPos()
+  init_cursor_pos = reaper.GetCursorPosition()
+end
+
+function RestoreCursorPos()
+  reaper.Main_OnCommand(40042, 0) -- Go to start of the project
+  reaper.MoveEditCursor(init_cursor_pos, false)
+end
+
+
+play_st = reaper.GetPlayState()
+if play_st == 4 or play_st == 5  then -- (4 and 5 = record)
+  reaper.Main_OnCommand(1013, 0) -- record (off)
+  reaper.Main_OnCommand(1016, 0) -- stop
+  metr_st = reaper.GetToggleCommandState(41745)
+  if metr_st == 1 then
+    reaper.Main_OnCommand(41745, 0) -- enable metronome (off)
+  end
+  
+  SaveCursorPos()
+  reaper.Main_OnCommand(40631, 0) -- go to end of time selection
+  reaper.Main_OnCommand(41311, 0) -- trim right edge of item to edit cursor
+  RestoreCursorPos()
+  
+elseif play_st == 1 then -- (1 = play)
+  reaper.Main_OnCommand(1016, 0) -- stop
+else
+  reaper.Main_OnCommand(1007, 0) -- play
+end
+
+reaper.Undo_EndBlock(script_title, -1)
