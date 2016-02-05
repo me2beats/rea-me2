a69a54495c813b6834212182c85d08e0c4092090
me2beats <prod.me2beats@gmail.com>
2016-02-05 18:49:53 +0500

    +

---
+++ items/Trim end of selected item to end of time selection.lua
@@ -0,0 +1,11 @@
+function nothing()
+end
+reaper.defer(nothing)
+
+it = reaper.GetSelectedMediaItem(0,0)
+if it ~= nil then
+  ts_start, ts_end = reaper.GetSet_LoopTimeRange(0, 0, 0, 0, 0)
+  if ts_end - ts_start > 0 then
+    reaper.ApplyNudge(0, 1, 3, 1, ts_end, false, 0)
+  end
+end
