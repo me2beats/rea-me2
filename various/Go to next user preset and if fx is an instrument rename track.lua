c8c349223b5c55b90f4794d9f81dbf07a19a13b0
me2beats <prod.me2beats@gmail.com>
2016-05-24 06:27:37 +0500

    +

---
+++ various/Go to next user preset and if fx is an instrument rename track.lua
@@ -0,0 +1,39 @@
+retval, trnum, _, fxnum = reaper.GetFocusedFX()
+if retval == 1 then
+  tr = reaper.GetTrack(0,trnum-1)  
+  preset_index = reaper.TrackFX_GetPresetIndex(tr, fxnum)
+  
+  filename = reaper.TrackFX_GetUserPresetFilename(tr, fxnum, '')
+  _, preset_name = reaper.TrackFX_GetPreset(tr, fxnum, '')
+  
+  os = reaper.GetOS()
+  if os == 'Win32' or 'Win64' then
+    filename_ok = string.gsub (filename, [[\]], [[\\]])
+  else
+    filename_ok = filename
+  end
+  file = io.open(filename_ok, "r")
+  if file ~= nil then
+    x = file:read('*a')
+    file:close()
+    presets = tonumber(x:match("NbPresets=(%d*)"))
+
+    reaper.Undo_BeginBlock()
+
+    if preset_index == presets-1 then
+      reaper.TrackFX_SetPresetByIndex(tr, fxnum, 0)
+    else
+      reaper.TrackFX_SetPresetByIndex(tr, fxnum, preset_index+1)
+    end
+    
+    instr_num = reaper.TrackFX_GetInstrument(tr)
+    
+    if instr_num == fxnum then
+      
+      _, new_preset_name = reaper.TrackFX_GetPreset(tr, fxnum, '')
+      reaper.GetSetMediaTrackInfo_String(tr, "P_NAME", new_preset_name, true)
+    end  
+      
+    reaper.Undo_EndBlock('', 2)
+  end
+end
