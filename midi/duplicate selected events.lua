3702cc19df351bdb6ffa9cc5748a3e652d8ecc41
me2beats <prod.me2beats@gmail.com>
2016-05-26 20:35:08 +0500

    upgraded

--- midi/duplicate selected events.lua
+++ midi/duplicate selected events.lua
@@ -8,36 +8,26 @@ function RestoreLoopTimesel()
   reaper.GetSet_LoopTimeRange(1, 1, init_start_loop, init_end_loop, 0)
 end
 
-function nothing()
-end
+function nothing() end
 
 take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
 if take ~= nil then
-      
+
   retval, notes, ccs, sysex = reaper.MIDI_CountEvts(take)
-  x = 0
+
   for k = 0, notes-1 do
     retval, sel, muted, startppqposOut, endppqposOut, chan, pitch, vel = reaper.MIDI_GetNote(take, k)
-    if sel == true then
-    
-      script_title = "duplicate selected events"
-      reaper.Undo_BeginBlock()
-    
---      reaper.MB('sel == true','123',0)
-      SaveLoopTimesel()
-      reaper.MIDIEditor_LastFocused_OnCommand(40752, false) -- set time selection to selected notes
-      reaper.MIDIEditor_LastFocused_OnCommand(40883, false) -- duplicate events within time selection
-      RestoreLoopTimesel()
-    
-      reaper.Undo_EndBlock(script_title, -1)
+    if sel == true then x = 1 break end
+  end
+  
+  if x == 1 then
+    reaper.Undo_BeginBlock()
+    SaveLoopTimesel()
+    reaper.MIDIEditor_LastFocused_OnCommand(40752, false) -- set time selection to selected notes
+    reaper.MIDIEditor_LastFocused_OnCommand(40883, false) -- duplicate events within time selection
+    RestoreLoopTimesel()
+    reaper.Undo_EndBlock("duplicate selected events", -1)
 
-      x = 1
-      break
-    end
-  end
-  if x == 0 then
-    reaper.defer(nothing)
-  end
-else
-  reaper.defer(nothing)
-end
+  else reaper.defer(nothing) end
+
+else reaper.defer(nothing) end
