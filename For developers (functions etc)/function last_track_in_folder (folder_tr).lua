b4ee2af61893ff62531b3d1d20f2f3b4363efecb
me2beats <prod.me2beats@gmail.com>
2016-06-13 10:39:22 +0500

    +

---
+++ For developers (functions etc)/function last_track_in_folder (folder_tr).lua
@@ -0,0 +1,10 @@
+function last_track_in_folder (folder_tr)
+  local dep = reaper.GetTrackDepth(folder_tr)
+  local num = reaper.GetMediaTrackInfo_Value(folder_tr, 'IP_TRACKNUMBER')
+  local tracks = reaper.CountTracks(0)
+  for i = num+1, tracks, 1 do
+    if reaper.GetTrackDepth(reaper.GetTrack(0,i-1)) <= dep then last = reaper.GetTrack(0,i-2) break end
+  end
+  if last == nil then last = reaper.GetTrack(0, tracks-1) end
+  return last
+end
