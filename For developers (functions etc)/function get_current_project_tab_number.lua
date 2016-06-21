e8c3768b570419bfecf5255410fed7e2bacb0b93
me2beats <prod.me2beats@gmail.com>
2016-06-21 19:13:54 +0500

    upd

--- For developers (functions etc)/function get_current_project_tab_number.lua
+++ For developers (functions etc)/function get_current_project_tab_number.lua
@@ -1,13 +1,13 @@
 function get_current_project_tab_number ()
+  retval = nil
   for p = 1, 1000 do
     retval = reaper.EnumProjects(p, '')
-    if not retval then x = p break end
+    if not retval then projects = p break end
   end
   local retval_0  = reaper.EnumProjects(-1, '')
-  for p = 1, x do
+  for p = 1, projects do
     retval  = reaper.EnumProjects(p-1, '')
     if retval == retval_0 then cur_proj_tab = p break end
   end
-  x, retval = nil, nil
-  return cur_proj_tab
+  retval = nil; return cur_proj_tab
 end
