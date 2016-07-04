ece00b515b1df0956051310739a14e48d81d4dba
me2beats <prod.me2beats@gmail.com>
2016-07-04 16:13:10 +0500

    +

---
+++ Track FX (presets etc)/add fx by name to selected track.lua
@@ -0,0 +1,9 @@
+function nothing() end
+
+track = reaper.GetSelectedTrack(0,0)
+if track then
+  reaper.Undo_BeginBlock()
+  retval, fx_name = reaper.GetUserInputs("Add FX to track by name", 1, "FX name:", "")
+  reaper.TrackFX_AddByName(track, fx_name, 0, -1)
+  reaper.Undo_EndBlock('add fx by name to sel track', -1)
+else reaper.defer(nothing) end
---
+++ Track FX (presets etc)/add fx by name to track under mouse.lua
@@ -0,0 +1,9 @@
+function nothing() end
+
+track = reaper.BR_TrackAtMouseCursor()
+if track then
+  reaper.Undo_BeginBlock()
+  retval, fx_name = reaper.GetUserInputs("Add FX to track by name", 1, "FX name:", "")
+  reaper.TrackFX_AddByName(track, fx_name, 0, -1)
+  reaper.Undo_EndBlock('add fx by name to track under mouse', -1)
+else reaper.defer(nothing) end
