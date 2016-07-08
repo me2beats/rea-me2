2968224927d91e447ed2e473d92c2fb2c6f645c9
me2beats <prod.me2beats@gmail.com>
2016-07-08 15:35:09 +0500

    +

---
+++ various/First letter upper/First letter Upper - tracks 0.95.lua
@@ -0,0 +1,39 @@
+function first_letter_upper_all_tracks()
+  tracks = reaper.CountTracks(0)
+  for i = 0, tracks-1 do
+    tr = reaper.GetTrack(0,i)
+    _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', '', 0)
+    if tr_name ~= '' then
+      if #tr_name > 1 then
+        tr_name = tr_name:sub(1,1):upper()..tr_name:sub(2,-1)
+      else tr_name = tr_name:sub(1,1):upper() end
+      reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', tr_name, 1)    
+    end      
+  end
+  reaper.defer(first_letter_upper_all_tracks)
+end
+
+-- Set ToolBar Button ON
+function SetButtonON()
+  _, _, sec, cmd = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd )
+  reaper.SetToggleCommandState( sec, cmd, 1 ) -- Set ON
+  reaper.RefreshToolbar2( sec, cmd )
+end
+--
+-- Set ToolBar Button OFF
+function SetButtonOFF()
+  _, _, sec, cmd = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd )
+  reaper.SetToggleCommandState( sec, cmd, 0 ) -- Set OFF
+  reaper.RefreshToolbar2( sec, cmd )
+end
+
+if reaper.MB('This script runs in defer. Continue?','Attention',1) == 1 then
+  
+  first_letter_upper_all_tracks()
+  
+  SetButtonON()
+  
+  reaper.atexit( SetButtonOFF )
+end
