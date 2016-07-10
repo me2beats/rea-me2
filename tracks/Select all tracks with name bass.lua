70edde5e586893d356cdcfb31d5c03735f2647fa
me2beats <prod.me2beats@gmail.com>
2016-07-11 01:04:46 +0500

    upd

--- tracks/Select all tracks with name bass.lua
+++ tracks/Select all tracks with name bass.lua
@@ -1,8 +1,10 @@
-tracks = reaper.CountTracks(0)
-reaper.Main_OnCommand(40297,0) -- unselect all tracks
+local tracks = reaper.CountTracks(0)
+if tracks > 0 then
+  reaper.Main_OnCommand(40297,0) -- unselect all tracks
 
-for i = 0, tracks-1 do
-  tr = reaper.GetTrack(0, i)
-  _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', '', 0)
-  if tr_name  == 'bass' then reaper.SetTrackSelected(tr, 1) end
+  for i = 0, tracks-1 do
+    local tr = reaper.GetTrack(0, i)
+    local _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', '', 0)
+    if tr_name  == 'bass' then reaper.SetTrackSelected(tr, 1) end
+  end
 end
