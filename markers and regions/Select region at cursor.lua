7fd122a52a0dbb0969498bcacb80d32cf8e65d1a
me2beats <prod.me2beats@gmail.com>
2016-07-12 18:22:45 +0500

    +

---
+++ markers and regions/Select region at cursor.lua
@@ -0,0 +1,7 @@
+markeridx, regionidx = reaper.GetLastMarkerAndCurRegion(0, reaper.GetCursorPosition())
+if regionidx then
+  reaper.Undo_BeginBlock()
+  _, _, x, y = reaper.EnumProjectMarkers3(0,regionidx)
+  reaper.GetSet_LoopTimeRange(1, 1, x, y, 0)
+  reaper.Undo_EndBlock("Select region at edit cursor", -1)
+end
