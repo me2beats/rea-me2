9f78f4dcf99f66549557cc17ce079f963ce775f5
me2beats <prod.me2beats@gmail.com>
2016-07-15 01:58:42 +0500

    fixed no selected items

--- items/Copy active take envelopes and Paste envelopes to active takes/1.0/Copy active take envelopes.lua
+++ items/Copy active take envelopes and Paste envelopes to active takes/1.0/Copy active take envelopes.lua
@@ -15,16 +15,18 @@ function esc (str)
 end
 
 item = reaper.GetSelectedMediaItem(0,0)
-_, chunk = reaper.GetItemStateChunk(item, '', 0)
-
-take = reaper.GetActiveTake(item)
-take_guid = reaper.BR_GetMediaItemTakeGUID(take)
-
-part = chunk:match(esc(take_guid)..'\n<SOURCE.->\n(.->)\nTAKE') or
-chunk:match(esc(take_guid)..'\n<SOURCE.->\n(.->)\n>')
-
-if part then
-  if part:match('^<.-ENV\n') then
-    reaper.SetExtState("me2beats_copy-paste", "take_envelopes", part, 0)
+if item then
+  _, chunk = reaper.GetItemStateChunk(item, '', 0)
+  
+  take = reaper.GetActiveTake(item)
+  take_guid = reaper.BR_GetMediaItemTakeGUID(take)
+  
+  part = chunk:match(esc(take_guid)..'\n<SOURCE.->\n(.->)\nTAKE') or
+  chunk:match(esc(take_guid)..'\n<SOURCE.->\n(.->)\n>')
+  
+  if part then
+    if part:match('^<.-ENV\n') then
+      reaper.SetExtState("me2beats_copy-paste", "take_envelopes", part, 0)
+    end
   end
 end
