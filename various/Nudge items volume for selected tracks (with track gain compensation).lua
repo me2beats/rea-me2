15770bc617a51ff47461781cc7849b0af3a4a8d4
me2beats <prod.me2beats@gmail.com>
2016-07-15 06:55:08 +0500

    +

--- various/Nudge items volume for selected tracks (with track gain compensation).lua
+++ various/Nudge items volume for selected tracks (with track gain compensation).lua
@@ -1,4 +1,4 @@
-local function nothing() end; local r = reaper
+local r = reaper; function nothing() end; function bla() r.defer(nothing) end;
 
 local tracks = r.CountSelectedTracks(0)
 if tracks > 0 then
@@ -14,17 +14,13 @@ if tracks > 0 then
           r.SetMediaTrackInfo_Value(tr, 'D_VOL', tr_vol*10^(0.05*(-delta_db)))
           for j = 0, items-1 do
             local it = r.GetTrackMediaItem(tr, j)
-            local takes = r.CountTakes(it)
-            for t = 0, takes-1 do
-              local take = r.GetTake(it, t)
-              local tk_vol = r.GetMediaItemTakeInfo_Value(take, 'D_VOL')
-              r.SetMediaItemTakeInfo_Value(take, 'D_VOL', tk_vol*10^(0.05*delta_db))
-            end
+            it_vol = r.GetMediaItemInfo_Value(it, 'D_VOL')
+            r.SetMediaItemInfo_Value(it, 'D_VOL', it_vol*10^(0.05*delta_db))
           end
         end        
       end
       r.PreventUIRefresh(-111); r.UpdateArrange()
       r.Undo_EndBlock('Nudge items volume for sel tracks (with compensation)', -1)
-    else r.defer(nothing) end
-  else r.defer(nothing) end
-else r.defer(nothing) end
+    else bla() end
+  else bla() end
+else bla() end
