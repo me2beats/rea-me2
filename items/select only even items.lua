9aa3052d818d0436bda321693304b44cd1bb1e36
me2beats <prod.me2beats@gmail.com>
2016-09-14 02:23:03 +0500

    +

--- items/select only even items.lua
+++ items/select only even items.lua
@@ -4,13 +4,13 @@ local items = r.CountSelectedMediaItems(0)
 if items > 1 then
   tb = {}
   for i = 0, items-1 do
-    if math.fmod (i, 2) == 0 then tb[#tb+1] = r.GetSelectedMediaItem(0,i) end
+    if math.fmod (i, 2) ~= 0 then tb[#tb+1] = r.GetSelectedMediaItem(0,i) end
   end
   if #tb > 0 then
     r.Undo_BeginBlock(); r.PreventUIRefresh(111)
     r.Main_OnCommand(40289,0) -- unselect all items
     for i = 1, #tb do r.SetMediaItemSelected(tb[i], 1) end
     r.UpdateArrange()
-    r.PreventUIRefresh(-111); r.Undo_EndBlock('Select only odd items', -1)
+    r.PreventUIRefresh(-111); r.Undo_EndBlock('Select only even items', -1)
   else bla() end
 else bla() end
