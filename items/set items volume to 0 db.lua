0638c6abb531d60e58da367ca47569b9582e8e3d
me2beats <prod.me2beats@gmail.com>
2016-10-10 02:36:07 +0500

    +

---
+++ items/set items volume to 0 db.lua
@@ -0,0 +1,12 @@
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+
+local items = r.CountSelectedMediaItems()
+if items > 0 then
+  r.Undo_BeginBlock()
+  for i = 0, items-1 do
+    local it = r.GetSelectedMediaItem(0,i)
+    r.SetMediaItemInfo_Value(it, 'D_VOL', 1)
+    r.UpdateItemInProject(it)
+  end
+  r.Undo_EndBlock('set items volume to 0 dB', -1)
+else bla() end
