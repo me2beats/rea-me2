7e73c8b12580f6067d8264a858721bb5e05d61b4
me2beats <prod.me2beats@gmail.com>
2016-11-13 23:23:20 +0500

    +

---
+++ tracks/remove muted tracks.lua
@@ -0,0 +1,19 @@
+r = reaper
+
+r.Undo_BeginBlock()
+
+r.PreventUIRefresh(-1)
+for i = 0, r.CountTracks()-1 do
+  tr = r.GetTrack(0,i)
+  if r.GetMediaTrackInfo_Value(tr, 'B_MUTE') == 1 then
+    if r.GetMediaTrackInfo_Value(tr, 'B_SHOWINTCP') == 1 then
+      r.SetTrackSelected(tr,1)
+    else r.SetTrackSelected(tr,0) end
+  else r.SetTrackSelected(tr,0) end
+end
+
+r.Main_OnCommand(40005,0) -- remove tracks
+
+r.PreventUIRefresh(1)
+
+r.Undo_EndBlock('remove muted tracks', -1)
