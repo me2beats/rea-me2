f1d0b02601ef3aa94d4cf613dcfe9c7a8892f3ef
me2beats <prod.me2beats@gmail.com>
2016-11-22 21:48:12 +0500

    +

---
+++ Track FX (presets etc)/Toggle all input FX bypass for selected track.lua
@@ -0,0 +1,11 @@
+local r = reaper
+
+tr = r.GetSelectedTrack(0,0)
+fx = r.TrackFX_GetRecCount(tr)
+
+for i = 0, fx-1 do
+  ii = i+16777216
+  local fx_enabled = r.TrackFX_GetEnabled(tr, ii)
+  r.TrackFX_SetEnabled(tr, ii, not fx_enabled)
+end
+
